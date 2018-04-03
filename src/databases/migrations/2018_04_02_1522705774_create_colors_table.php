4f87aa1c3ac22af7c17b813db053c99b1d9ffa9d
Mike Wall <daikazu@gmail.com>
2018-04-03 13:36:07 -0400

    wip

--- src/databases/migrations/2018_04_02_1522705774_create_colors_table.php
+++ src/databases/migrations/2018_04_02_1522705774_create_colors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateColorbookTable extends Migration
+class CreateColorsTable extends Migration
 {
 
     public function up()
