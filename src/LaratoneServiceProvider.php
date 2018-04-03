b03fa729fb6dac5cd96ae762e2bfae0338ee31c4
Mike Wall <daikazu@gmail.com>
2018-04-03 15:50:45 -0400

    wip

--- src/LaratoneServiceProvider.php
+++ src/LaratoneServiceProvider.php
@@ -12,7 +12,7 @@ class LaratoneServiceProvider extends ServiceProvider
     {
         $app = $this->app;
 
-        $this->publishes([__DIR__ . '/config/daikazu/laratone.php' => config_path('daikazu/laratone.php')], 'laratone-config');
+        $this->publishes([__DIR__ . '/config/daikazu/laratone.php' => config_path('/daikazu/laratone.php')], 'laratone-config');
         $this->loadMigrationsFrom(__DIR__ . '/databases/migrations');
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
 
@@ -21,7 +21,7 @@ class LaratoneServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/config/laratone.php', 'laratone');
+        $this->mergeConfigFrom(__DIR__ . '/config/daikazu/laratone.php', 'daikazu.laratone');
 
 
     }
