206b0fc42cc2cbccfbc91c791286b63ccb89f8ef
Mike Wall <daikazu@gmail.com>
2025-04-29 09:32:38 -0400

    wip

--- src/Laratone.php
+++ src/Laratone.php
@@ -2,6 +2,7 @@
 
 namespace Daikazu\Laratone;
 
+use Daikazu\Laratone\Models\Color;
 use Daikazu\Laratone\Models\ColorBook;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Cache;
@@ -62,5 +63,79 @@ class Laratone
     {
         Cache::forget('laratone.color_books');
         Cache::tags(['laratone'])->flush();
+    }
+
+    /**
+     * Add a color to a color book.
+     *
+     * @param  ColorBook  $colorBook  The color book to add the color to
+     * @param  array  $colorData  The color data to add
+     * @return Color The newly created color
+     */
+    public function addColorToBook(ColorBook $colorBook, array $colorData): Color
+    {
+        $color = $colorBook->colors()->create($colorData);
+        $this->clearCache();
+
+        return $color;
+    }
+
+    /**
+     * Add multiple colors to a color book.
+     *
+     * @param  ColorBook  $colorBook  The color book to add the colors to
+     * @param  array  $colorsData  Array of color data arrays
+     * @return Collection<int, Color> The newly created colors
+     */
+    public function addColorsToBook(ColorBook $colorBook, array $colorsData): Collection
+    {
+        $colors = $colorBook->colors()->createMany($colorsData);
+        $this->clearCache();
+
+        return $colors;
+    }
+
+    /**
+     * Update a color in a color book.
+     *
+     * @param  Color  $color  The color to update
+     * @param  array  $colorData  The new color data
+     * @return bool Whether the update was successful
+     */
+    public function updateColor(Color $color, array $colorData): bool
+    {
+        $result = $color->update($colorData);
+        $this->clearCache();
+
+        return $result;
+    }
+
+    /**
+     * Delete a color from a color book.
+     *
+     * @param  Color  $color  The color to delete
+     * @return bool Whether the deletion was successful
+     */
+    public function deleteColor(Color $color): bool
+    {
+        $result = $color->delete();
+        $this->clearCache();
+
+        return $result;
+    }
+
+    /**
+     * Get all colors from a color book.
+     *
+     * @param  ColorBook  $colorBook  The color book to get colors from
+     * @return Collection<int, Color> The colors in the color book
+     */
+    public function getColorsFromBook(ColorBook $colorBook): Collection
+    {
+        return Cache::remember(
+            "laratone.color_book.{$colorBook->slug}.colors",
+            3600,
+            fn () => $colorBook->colors()->get()
+        );
     }
 }
