f9625bdf628439baf6e76f1ffef441a9eed20643
Mike Wall <daikazu@gmail.com>
2025-04-29 09:31:21 -0400

    wip

--- src/Laratone.php
+++ src/Laratone.php
@@ -3,16 +3,64 @@
 namespace Daikazu\Laratone;
 
 use Daikazu\Laratone\Models\ColorBook;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 
 class Laratone
 {
-    public function colorBooks(): \Illuminate\Database\Eloquent\Collection|array
+    /**
+     * Get all color books with their associated colors.
+     *
+     * @return Collection<int, ColorBook>
+     */
+    public function colorBooks(): Collection
     {
-        return ColorBook::with('colors')->get();
+        return Cache::remember('laratone.color_books', 3600, function () {
+            return ColorBook::with('colors')->get();
+        });
     }
 
-    public function colorBookBySlug($colorBookSlug)
+    /**
+     * Get a color book by its slug.
+     *
+     * @param  string  $colorBookSlug  The slug of the color book to retrieve
+     * @return ColorBook|null The color book if found, null otherwise
+     */
+    public function colorBookBySlug(string $colorBookSlug): ?ColorBook
     {
-        return ColorBook::slug($colorBookSlug)->first();
+        return Cache::remember(
+            "laratone.color_book.{$colorBookSlug}",
+            3600,
+            function () use ($colorBookSlug) {
+                return ColorBook::slug($colorBookSlug)->first();
+            }
+        );
+    }
+
+    /**
+     * Create a new color book.
+     *
+     * @param  string  $name  The name of the color book
+     * @param  string|null  $slug  Optional slug. If not provided, will be generated from the name
+     * @return ColorBook The newly created color book
+     */
+    public function createColorBook(string $name, ?string $slug = null): ColorBook
+    {
+        $slug = $slug ?? Str::slug($name);
+
+        return ColorBook::create([
+            'name' => $name,
+            'slug' => $slug,
+        ]);
+    }
+
+    /**
+     * Clear all cached color book data.
+     */
+    public function clearCache(): void
+    {
+        Cache::forget('laratone.color_books');
+        Cache::tags(['laratone'])->flush();
     }
 }
